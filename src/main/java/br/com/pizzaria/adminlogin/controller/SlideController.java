7f0fb34de0aacb3720e3a177d35e803d79b9d69a
gustavozlb <zimermanng66@gmail.com>
2026-03-24 16:32:01 -0300

    Melhorando validação de imagem

--- src/main/java/br/com/pizzaria/adminlogin/controller/SlideController.java
+++ src/main/java/br/com/pizzaria/adminlogin/controller/SlideController.java
@@ -16,11 +16,42 @@ public class SlideController {
 
     private final SlideRepository slideRepository;
 
+    private String detectImageType(byte[] bytes) {
+    // JPEG
+    if (bytes.length > 3 &&
+        (bytes[0] & 0xFF) == 0xFF &&
+        (bytes[1] & 0xFF) == 0xD8 &&
+        (bytes[2] & 0xFF) == 0xFF) {
+        return "image/jpeg";
+    }
+
+    // PNG
+    if (bytes.length > 4 &&
+        (bytes[0] & 0xFF) == 0x89 &&
+        (bytes[1] & 0xFF) == 0x50 &&
+        (bytes[2] & 0xFF) == 0x4E &&
+        (bytes[3] & 0xFF) == 0x47) {
+        return "image/png";
+    }
+
+    // WEBP
+    if (bytes.length > 12 &&
+        bytes[0] == 'R' && bytes[1] == 'I' &&
+        bytes[2] == 'F' && bytes[3] == 'F' &&
+        bytes[8] == 'W' && bytes[9] == 'E' &&
+        bytes[10] == 'B' && bytes[11] == 'P') {
+        return "image/webp";
+    }
+    return null; 
+
+    }   
+    
+
     public SlideController(SlideRepository slideRepository) {
         this.slideRepository = slideRepository;
     }
 
-    // Buscar slides por categoria (com fallback)
+    // Buscar slides por categoria
     @GetMapping("/slides")
     public List<Slide> getSlidesByCategory(@RequestParam(required = false) String category) {
         if (category == null || category.isBlank()) {
@@ -39,6 +70,13 @@ public class SlideController {
             if (file.isEmpty()) {
                 return ResponseEntity.badRequest().body("Imagem não pode ser vazia.");
             }
+
+            byte[] bytes = file.getBytes();
+            String contentType = detectImageType(bytes);
+
+            if (contentType == null) {
+            return ResponseEntity.badRequest().body("Formato de imagem inválido.");
+        }
 
             Slide slide = new Slide();
             slide.setCategory(category);
